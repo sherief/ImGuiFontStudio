e9981dae388a4fcab0d6137c91a4f651fccc3de5
Aiekick <aiekick@funparadigm.com>
2020-09-19 15:33:03 +0200

    fix for unix

--- cmake/freetype.cmake
+++ cmake/freetype.cmake
@@ -1,3 +1,8 @@
+option(FT_WITH_ZLIB OFF)
+option(FT_WITH_BZIP2 OFF)
+option(FT_WITH_PNG OFF)
+option(FT_WITH_HARFBUZZ OFF)
+option(FT_WITH_BROTLI OFF)
 add_subdirectory(${CMAKE_SOURCE_DIR}/3rdparty/freetype2)
 
 set_target_properties(freetype PROPERTIES FOLDER 3rdparty)
